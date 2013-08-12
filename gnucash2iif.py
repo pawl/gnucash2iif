916b51cc55fcd8b41f7505aa6659eff54f605618
Paul Brown <paul90brown@gmail.com>
2013-08-12 01:33:51 -0500

    Check if first column is date

--- gnucash2iif.py
+++ gnucash2iif.py
@@ -1,6 +1,18 @@
 import csv
-import sys
-with open(sys.argv[1], 'rb') as f:
+import re
+
+def CheckDate(dateString):
+        '''Checks a date if it is in a valid format'''
+        r = re.compile('.*/.*/.*')
+        if r.match(dateString) is not None:
+            return True
+        else:
+            return False
+
+with open('generalledger.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        print row
+        if CheckDate(row[0]):
+            print "this is a date"
+        else:
+            print row
